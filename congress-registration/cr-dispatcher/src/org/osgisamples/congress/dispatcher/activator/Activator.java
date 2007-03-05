b228e2dda39035eea1b882fbd66d423bf92c779e
buijze <buijze@45f6cf87-e128-0410-99ec-830c0de225ea>
2007-03-05 20:30:50 +0000

    Modified dispatcher. It will now look for services that handle the root element of the XML request (in the envelope body) and dispatch the request as Document to that service.

--- congress-registration/cr-dispatcher/src/org/osgisamples/congress/dispatcher/activator/Activator.java
+++ congress-registration/cr-dispatcher/src/org/osgisamples/congress/dispatcher/activator/Activator.java
@@ -2,6 +2,9 @@ package org.osgisamples.congress.dispatcher.activator;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgisamples.congress.dispatcher.SOAPDispatcher;
+import org.osgisamples.congress.servicelocator.ServiceLocator;
+import org.osgisamples.congress.servicelocator.XmlWebServiceProviderLocator;
 
 public class Activator implements BundleActivator {
 
@@ -10,8 +13,8 @@ public class Activator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
-		// todo: listen for CongressServices
-		// keep a list of active congress services.
+		ServiceLocator locator = new XmlWebServiceProviderLocator(context);
+		SOAPDispatcher.setServiceLocator(locator);
 	}
 
 	/*
