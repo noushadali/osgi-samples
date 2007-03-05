1a15c5fc42867d273e92919b38e5cc9cfb4b5e57
buijze <buijze@45f6cf87-e128-0410-99ec-830c0de225ea>
2007-03-05 20:30:50 +0000

    Modified dispatcher. It will now look for services that handle the root element of the XML request (in the envelope body) and dispatch the request as Document to that service.

--- trunk/congress-registration/cr-dispatcher/src/org/osgisamples/congress/dispatcher/SOAPDispatcher.java
+++ trunk/congress-registration/cr-dispatcher/src/org/osgisamples/congress/dispatcher/SOAPDispatcher.java
@@ -1,12 +1,49 @@
 package org.osgisamples.congress.dispatcher;
 
 import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPFault;
 
 import org.apache.axis.message.SOAPEnvelope;
+import org.osgisamples.congress.servicelocator.ServiceLocator;
+import org.osgisamples.provider.XmlWebServiceProvider;
 
 public class SOAPDispatcher implements Dispatcher {
 
-	public void doService(SOAPEnvelope req, SOAPEnvelope resp) throws SOAPException  {
-		System.out.println("Received SOAP message of type: " + req.getBody().getChildNodes().item(0).getLocalName());
+	private static ServiceLocator locator;
+
+	public static void setServiceLocator(ServiceLocator locator) {
+		SOAPDispatcher.locator = locator; 
 	}
+	
+	public void doService(SOAPEnvelope req, SOAPEnvelope resp) throws SOAPException  {
+		String requestType = req.getBody().getChildNodes().item(0).getLocalName();
+		System.out.println("Received SOAP message of type: " + requestType);
+		try
+		{
+			XmlWebServiceProvider service = locator.findService(requestType);
+			if(service != null)
+			{
+				System.out.println("Service found throug locator: " + service.getClass().getName());
+				resp.getBody().addDocument(service.doService(req.getAsDocument()));
+			}
+			else
+			{
+				createError(resp, "No service found");
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace(System.out);
+			throw new RuntimeException(ex);
+		}
+	}
+
+	private void createError(SOAPEnvelope response, String errorText) throws SOAPException {
+		
+		SOAPFault fault = response.getBody().addFault();
+		fault.setFaultCode("ServiceNotFound");
+		fault.setFaultString(errorText);
+	}
+
+	
 }
