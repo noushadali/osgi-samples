318ea32289e80a268f8a5f5c5749026f890a4a81
Jettro.Coenradie <Jettro.Coenradie@45f6cf87-e128-0410-99ec-830c0de225ea>
2007-03-25 20:36:41 +0000

    added the initial version of the business services

---
+++ trunk/congress-registration/cr-business-service/src/com/osgisamples/congress/business/impl/CongressManagerImpl.java
@@ -0,0 +1,13 @@
+package com.osgisamples.congress.business.impl;
+
+import com.osgisamples.congress.business.CongressManager;
+import com.osgisamples.congress.domain.Congress;
+
+public class CongressManagerImpl implements CongressManager {
+
+	public void storeCongress(Congress congress) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
