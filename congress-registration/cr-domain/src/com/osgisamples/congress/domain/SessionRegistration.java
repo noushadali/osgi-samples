78d1a15645606f6f372631326eed652e809d2827
Jettro.Coenradie <Jettro.Coenradie@45f6cf87-e128-0410-99ec-830c0de225ea>
2007-03-28 07:45:20 +0000

    Implemented the toString/HashCode/Equals methods

--- congress-registration/cr-domain/src/com/osgisamples/congress/domain/SessionRegistration.java
+++ congress-registration/cr-domain/src/com/osgisamples/congress/domain/SessionRegistration.java
@@ -1,5 +1,9 @@
 package com.osgisamples.congress.domain;
 
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 public abstract class SessionRegistration extends BaseDomain {
 	private Session registeredSession;
 
@@ -10,4 +14,35 @@ public abstract class SessionRegistration extends BaseDomain {
 	public void setRegisteredSession(Session registeredSession) {
 		this.registeredSession = registeredSession;
 	}
+
+	@Override
+	public boolean equals(Object obj) {
+		boolean returnValue = false;
+		if (obj instanceof Congress) {
+			if (this == obj) {
+				returnValue = true;
+			} else {
+				Session sessionToCheck = (Session) obj;
+				returnValue = new EqualsBuilder()
+					.append(registeredSession, sessionToCheck)
+					.isEquals();
+			}
+		}
+		return returnValue;
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17,23)
+		.append(registeredSession)
+		.toHashCode();
+	}
+
+	@Override
+	public String toString() {
+		return new ToStringBuilder(this)
+		.append("Registered Session",registeredSession)
+		.toString();
+	}
+	
 }
