253ce22e38ec7b1b8ea0bd6d55ca884893e6a6e2
buijze <buijze@45f6cf87-e128-0410-99ec-830c0de225ea>
2007-04-02 16:11:20 +0000

    Refactoring. Removed unnecessary JAR files. Moved wsdd file to WEB-INF folder, where Axis looks for it.

--- trunk/congress-registration/cr-dispatcher/src/com/osgisamples/congress/servicelocator/XmlWebServiceProviderLocator.java
+++ trunk/congress-registration/cr-dispatcher/src/com/osgisamples/congress/servicelocator/XmlWebServiceProviderLocator.java
@@ -3,6 +3,8 @@ package com.osgisamples.congress.servicelocator;
 import java.util.Arrays;
 import java.util.Iterator;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
@@ -12,6 +14,8 @@ import com.osgisamples.congress.provider.XmlWebServiceProvider;
 
 public class XmlWebServiceProviderLocator extends ServiceTracker implements
 ServiceLocator {
+	
+	private static final Log log = LogFactory.getLog(XmlWebServiceProviderLocator.class);
 
 	public XmlWebServiceProviderLocator(BundleContext bundleContext) throws InvalidSyntaxException {
 		super(bundleContext, bundleContext.createFilter("(&(objectClass=" + XmlWebServiceProvider.class.getName() + ")(rootElement=*))"), null);
@@ -29,12 +33,12 @@ ServiceLocator {
 			while(it.hasNext()) {
 				ServiceReference ref = it.next();
 				if(rootElement.equals(ref.getProperty("rootElement"))) {
-					System.out.println("Found the right service, now check for the version");
+					log.info("Found the right service, now check for the version");
 					if (version == null) {
-						System.out.println("No special version requested");
+						log.info("No special version requested");
 						return (XmlWebServiceProvider) getService(ref);
 					} else if (version.equals(ref.getProperty("version"))){
-						System.out.println("Found the requested version");
+						log.info("Found the requested version");
 						return (XmlWebServiceProvider) getService(ref);
 					}
 				}
