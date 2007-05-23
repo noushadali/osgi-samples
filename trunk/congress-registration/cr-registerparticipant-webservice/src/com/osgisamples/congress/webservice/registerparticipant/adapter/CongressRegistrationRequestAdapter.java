f1ecd174009c7802bcd46e06137573415103e905
Jettro.Coenradie <Jettro.Coenradie@45f6cf87-e128-0410-99ec-830c0de225ea>
2007-05-23 19:42:33 +0000

    made the changes of version 2 to the normal or version 1. I am also committing the changes of two. In a while we can delete this project

--- trunk/congress-registration/cr-registerparticipant-webservice/src/com/osgisamples/congress/webservice/registerparticipant/adapter/CongressRegistrationRequestAdapter.java
+++ trunk/congress-registration/cr-registerparticipant-webservice/src/com/osgisamples/congress/webservice/registerparticipant/adapter/CongressRegistrationRequestAdapter.java
@@ -1,8 +1,15 @@
 package com.osgisamples.congress.webservice.registerparticipant.adapter;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 import com.osgisamples.congress.domain.Congress;
 import com.osgisamples.congress.domain.Registrant;
+import com.osgisamples.congress.domain.Session;
 import com.osgisamples.congress.schema.CongressRegistrationRequest;
+import com.osgisamples.congress.schema.Sessions;
 import com.osgisamples.congress.webservice.registerparticipant.dataholder.CongressRegistrationRequestDataHolder;
 
 public class CongressRegistrationRequestAdapter {
@@ -11,7 +18,7 @@ public class CongressRegistrationRequestAdapter {
 		CongressRegistrationRequestDataHolder dataHolder = new CongressRegistrationRequestDataHolder();
 		dataHolder.setCongress(adaptCongress(request.getCongress()));
 		dataHolder.setRegistrant(adaptRegistrant(request.getRegistrant()));
-		
+		dataHolder.setSessions(adaptSessions(request.getSessions()));
 		return dataHolder;
 	}
 
@@ -32,4 +39,18 @@ public class CongressRegistrationRequestAdapter {
 		return congress;
 	}
 	
+	private Set<Session> adaptSessions(Sessions xmlSessions) {
+		Set<Session> sessions = new HashSet<Session>();
+		List<com.osgisamples.congress.schema.Session> xmlSessionsList = xmlSessions.getSession();
+		for (Iterator<com.osgisamples.congress.schema.Session> iter = xmlSessionsList.iterator(); iter.hasNext();) {
+			com.osgisamples.congress.schema.Session xmlSession = iter.next();
+			Session session = new Session();
+			if (xmlSession.getSessionId() != null) {
+				session.setId(new Long(xmlSession.getSessionId()));
+			}
+			session.setName(xmlSession.getSessionName());
+			sessions.add(session);
+		}
+		return sessions;
+	}
 }
